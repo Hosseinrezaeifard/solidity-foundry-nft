57d5d70d30f5c20e2c69231365d1bdddce9affc9
HosseinRezaeiFard <huseinrezaeif99@gmail.com>
2025-06-03 13:36:28 -0700

    Implement first test

--- test/BasicNftTest.s.sol
+++ test/BasicNftTest.s.sol
@@ -2,15 +2,28 @@
 
 pragma solidity ^0.8.20;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
 import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
 import {BasicNft} from "../src/BasicNft.sol";
 
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
-    function setup() public {
+    address public USER = makeAddr("user");
+    string public constant MOCK_URI =
+        "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/";
+
+    function setUp() public {
         deployer = new DeployBasicNft();
-        BasicNft = deployer.run();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public view {
+        string memory expectedName = "Dogie";
+        string memory actualName = basicNft.name();
+        assert(
+            keccak256(abi.encodePacked(expectedName)) ==
+                keccak256(abi.encodePacked(actualName))
+        );
     }
 }
