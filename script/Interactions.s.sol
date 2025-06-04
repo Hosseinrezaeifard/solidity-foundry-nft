5370c507ccc809d31887251deae0d5586e0fd071
HosseinRezaeiFard <huseinrezaeif99@gmail.com>
2025-06-04 10:15:03 -0700

    Implement interactions file & integration tests

---
+++ script/Interactions.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+
+contract MintBasicNft is Script {
+    string public constant PUG_URI =
+        "ipfs://bafybeiho2obyugmhnddgoxhd5dqp2pl23nmfz3lmvdux42ggp5nt5ttepm";
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNft",
+            block.chainid
+        );
+
+        mintNftOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(PUG_URI);
+        vm.stopBroadcast();
+    }
+}
